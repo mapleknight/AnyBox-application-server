67d4ae91e23fa8c24d014cd54b1983a040e702a0
mapleknight <karlwang1118@gmail.com>
2015-05-29 20:05:57 -0400

    change exception location

---
+++ src/main/java/com/anybox/Exception/RepeatedUpdateException.java
@@ -0,0 +1,14 @@
+package com.anybox.Exception;
+
+public class RepeatedUpdateException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7415972151860447098L;
+	
+	public RepeatedUpdateException(){
+		super();
+	}
+
+}
